8bb75a39974507a2316bbf5eebdd6058fa5e83d7
sang seok lim <sangseok.lim@sk.com>
2013-06-23 18:04:17 +0900

    initial drop:WebGL데모

--- js/presentation.js
+++ js/presentation.js
@@ -35,56 +35,29 @@ var pageAnimationHandler = {
 
 //page 별 animation hanlder를 기술 후 일괄 등록
 pageAnimationHandler.init({
-    tstore30: function(){
-        $('#tstore30 iframe').each( function( index, elem ){
+    helloRacer: function(){
+        $('#helloRacer iframe').each( function( index, elem ){
             var that = this;
             setTimeout( function(){
                 $(that).attr( "src", $( that ).attr( 'data-src') );
             }, 2000);
+        });
+    },
+    bookshelf: function(){
+        $('#bookshelf iframe').each( function( index, elem ){
+            var that = this;
+            setTimeout( function(){
+                $(that).attr( "src", $( that ).attr( 'data-src') );
+            }, 2000);
+        });
+        $('#bookshelf .title').off().on('click',function(){
+            $('#bookshelf div' ).toggleClass("animating");
+            $('#bookshelf .title').html("CSS3D: rotate y축")
         });
     },
     linkedinIssue: function(){
         $('#linkedinIssue img').off().on('click', function(){
             alert("제대로 된 Tool이 없어요!");
-        });
-    },
-    timeline: function(){
-        $('#timeline .timelineImg').off().on('click', function(){
-            $(this).toggleClass('moveUp');
-        });
-    },
-    timelineDetail: function(){
-        $('#timelineDetail .leftImg').off().on('click', function(){
-            $('#timelineDetail .leftImg').toggleClass('scaleUp2x');
-        });
-        $('#timelineDetail .rightImg').off().on('click', function(){
-            $('#timelineDetail .rightImg').toggleClass('scaleUp2x');
-        });
-    },
-    timelinePractice: function(){
-        $('#timelinePractice iframe').each( function( index, elem ){
-            var that = this;
-            setTimeout( function(){
-                $(that).attr( "src", $( that ).attr( 'data-src') );
-            }, 2000);
-        });
-    },
-    continuousPainting: function(){
-        $('#continuousPainting .contImg').off().on('click', function(){
-            $('#continuousPainting .contImg2').toggleClass('moveUp');
-        });
-    },
-    useTransitionbyInspector: function(){
-        $('#useTransitionbyInspector .leftImg').off().on('click', function(){
-            $('#useTransitionbyInspector .leftImg').toggleClass('scaleUp2x');
-        });
-        $('#useTransitionbyInspector .rightImg').off().on('click', function(){
-            $('#useTransitionbyInspector .rightImg').toggleClass('scaleUp2x');
-        });
-    },
-    paintingPath: function(){
-        $('#paintingPath .helloWorld').off().on('click', function(){
-            $('#paintingPath .helloWorld').toggleClass('rotateImg');
         });
     }
 });
